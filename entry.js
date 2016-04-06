a16af66a4c2ed8f81e22621cfaf8ea103b0b1472
David Soyez <david@dexnode.com>
2016-04-06 15:15:31 +0200

    exemple2

---
+++ entry.js
@@ -0,0 +1,6 @@
+var monModule = require('./monModule');
+
+var module1 = new monModule();
+document.write(module1.bonjour());
+document.write('<br>');
+document.write(module1.auRevoir());
---
+++ monModule.js
@@ -0,0 +1,11 @@
+function monModule() {
+  this.bonjour = function() {
+    return 'bonjour!';
+  }
+
+  this.auRevoir = function() {
+    return 'au revoir!';
+  }
+}
+
+module.exports = monModule;
